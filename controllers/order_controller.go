d8763d2380e4e2d4b0280f56e24ad5b7b462a411
Codart-7 <iconmoa@gmail.com>
2024-12-25 07:38:02 +0100

    order controllers

--- controllers/order_controller.go
+++ controllers/order_controller.go
@@ -8,6 +8,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// CreateOrder
 func CreateOrder(c *gin.Context) {
 	var order models.Order
 
@@ -25,6 +26,7 @@ func CreateOrder(c *gin.Context) {
 
 }
 
+// GetOrders
 func GetOrders(c *gin.Context) {
 	var orders []models.Order
 
@@ -36,6 +38,7 @@ func GetOrders(c *gin.Context) {
 	c.JSON(http.StatusOK, orders)
 }
 
+// GetOrderByID
 func GetOrderByID(c *gin.Context) {
 	var order models.Order
 	id := c.Param("id")
@@ -46,4 +49,45 @@ func GetOrderByID(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, order)
+}
+
+// UpdateOrder
+func UpdateOrder(c *gin.Context) {
+	var order models.Order
+	id := c.Param("id")
+
+	if err := config.DB.First(&order, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Order not found"})
+		return
+	}
+
+	if err := c.ShouldBindJSON(&order); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if err := config.DB.Model(&order).Where("id = ?", id).Updates(&order).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, order)
+}
+
+// DeleteOrder
+func DeleteOrder(c *gin.Context) {
+	var order models.Order
+	id := c.Param("id")
+
+	if err := config.DB.First(&order, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Order not found"})
+		return
+	}
+
+	if err := config.DB.Delete(&order).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Order deleted successfully"})
 }
